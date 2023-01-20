a74468d0f50ab377cc979a0f84fb0533a840ccff
ylose <heungeob1003@gmail.com>
2023-01-20 15:02:59 +0900

    Scaffold로 SingleChildScrollView 감싸기

--- lib/home.dart
+++ lib/home.dart
@@ -27,29 +27,31 @@ class _HomePageState extends State<HomePage> {
   }
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 30.0),
-            alignment: Alignment.center,
-            child: Text(
-              "OCR APP",
-              style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.w700, fontSize: 20),
-              textAlign: TextAlign.center,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 30.0),
+              alignment: Alignment.center,
+              child: Text(
+                "OCR APP",
+                style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.w700, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          SizedBox(height: 15.0),
-          Container(
-              width: MediaQuery.of(context).size.width / 2,
-              child: ElevatedButton(
-                  onPressed: () => parsethetext(),
-                  child: Text('Upload a image',
-                      style: GoogleFonts.montserrat(
-                          fontSize: 20, fontWeight: FontWeight.w700))))
-        ],
-      ),
+            SizedBox(height: 15.0),
+            Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: ElevatedButton(
+                    onPressed: () => parsethetext(),
+                    child: Text('Upload a image',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 20, fontWeight: FontWeight.w700))))
+          ],
+        ),
+      )
     );
   }
 }
